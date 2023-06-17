aadc3b8e7e572baf31900f429ac94bcaffd1be5e
FahrulDems <damesfahrul2@gmail.com>
2023-06-17 19:27:23 +0700

    Add Refactoring From Connection

---
+++ RefactoringFromConnection/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+using Connection.Contexts;
+using Connection.Controllers;
+
+namespace Connection;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        new Koneksi().TesConnection();
+        new MenuUtama().FirstMenu();
+    }
+}
